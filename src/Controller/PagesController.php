1b69f8e3e01a897cd56b84fbf3b026b8a3da06e7
iamansaka <anssaka.mahamoudou@icloud.com>
2022-10-27 17:38:31 +0200

    :sparkles: Add legal mentions

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -8,9 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PagesController extends AbstractController
 {
-    #[Route('/maltraitance', name: 'app_abuse')]
+    #[Route('/maltraitance', name: 'app_abuse', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('pages/abuse.html.twig');
+    }
+
+    #[Route('/politique-de-cookies', name: 'app_policy_cookies', methods: ['GET'])]
+    public function policyCookie(): Response
+    {
+        return $this->render('pages/policy_cookie.html.twig');
+    }
+
+    #[Route('mentions-legales', name: 'app_mentions', methods: ['GET'])]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('pages/mentions.html.twig');
     }
 }
