5d6968c85e2dd357d531875de1fb11c29a69d4c4
iamansaka <anssaka.mahamoudou@icloud.com>
2022-10-31 15:35:07 +0100

    :construction: Implementation of the search bar on the articles page

--- webpack.config.js
+++ webpack.config.js
@@ -74,12 +74,13 @@ Encore
 
   // enables Sass/SCSS support
   .enableSassLoader();
+// .enableVueLoader();
 
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
 
 // uncomment if you use React
-//.enableReactPreset()
+// .enableReactPreset();
 
 // uncomment to get integrity="..." attributes on your script & link tags
 // requires WebpackEncoreBundle 1.4 or higher
